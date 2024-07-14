83180b2880722db2cea33c6928e613e5b010a218
Snizhana_K <snizhana.knysh@intellecteu.com>
2024-07-14 23:35:00 +0300

    add distance converter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import app.ConverterWeight;
 import lecture2.ConverterDistance;
 
 public class Main {
@@ -6,14 +7,22 @@ public class Main {
         double kgs = 5;
         double pnds = 12;
 
-        System.out.println("Converting " + kgs + " kgs into pounds. The result is: " + ConverterWeight.convKgsToPounds(kgs));
-        System.out.println("Converting " + pnds + " pounds into kgs. The result is: " + ConverterWeight.convPoundsToKgs(pnds));
         double km = 10.0;
         double miles = 20.0;
-        System.out.println(ConverterDistance.convKmToMiles(km));
-        System.out.println(ConverterDistance.convMilesToKm(miles));
-        System.out.println(ConverterDistance.convKmToMiles(km));
-        System.out.println(ConverterDistance.convMilesToKm(miles));
+
+        System.out.println("WEIGHT CONVERTER");
+        System.out.println();
+
+
+        System.out.println("Converting " + kgs + " kgs into pounds. The result is: " + ConverterWeight.convKgsToPounds(kgs));
+        System.out.println("Converting " + pnds + " pounds into kgs. The result is: " + ConverterWeight.convPoundsToKgs(pnds));
+
+        System.out.println();
+        System.out.println("DISTANCE CONVERTER");
+        System.out.println();
+
+        System.out.println("Converting " + km + " km into miles. The result is: " + ConverterDistance.convKmToMiles(km));
+        System.out.println("Converting " + miles + " miles into kms. The result is: " + ConverterDistance.convMilesToKm(miles));
     }
 
 }
